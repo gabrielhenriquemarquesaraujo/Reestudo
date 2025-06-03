1b7601a0a429d8c00238c62fa57400e8d22e6d22
Gabriel Henrique <72325571+gabrielhenriquemarquesaraujo@users.noreply.github.com>
2025-06-03 19:32:39 -0300

    Add files via upload

---
+++ Exercicio1.js
@@ -0,0 +1,7 @@
+let nomecidade = "Recife";
+let populacao = "1500000"
+let capital = true;
+
+console.log("Cidade:", nomecidade);
+console.log("Numero da População:", populacao);
+console.log("É capital?", capital);
